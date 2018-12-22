13dcf9ca919cd849d7dca6e0e670e7e9bc947250
Alexander Dydychkin <AlexanderDydychkin@yandex.ru>
2018-12-23 00:41:01 +0300

    Update and rename build.sh to scripts/build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Build test project"
-git clone https://github.com/GoAlexander/infrastructure-test.git project
-cd project
-g++ main.cpp -o main.out
---
+++ scripts/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Build test project"
+g++ main.cpp -o main.out
