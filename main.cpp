4ea2c4f05ab1b3ca996e58c1ba968aa676f22dcc
Alexander Dydychkin <AlexanderDydychkin@yandex.ru>
2018-12-23 20:52:48 +0300

    Brokean main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main() 
-{ 
     cout << "Hello, world!" << endl;
     cout << "CI works!" << endl;
-    cout << "It is alive! :)" << endl;
+    
     return 0; 
 }
